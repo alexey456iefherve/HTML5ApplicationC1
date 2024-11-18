57114fb7c7013f41ce410a3cf0ca5fa061be1168
alex <gellioss15@gmail.com>
2024-11-19 02:06:32 +0700

    Initial commit

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Html5Tests/karma.conf.js to edit this template
+ */
+
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
+        
+        files: [
+        ],
+        
+        exclude: [
+        ],
+        
+        autoWatch: true,
+        
+        frameworks: [
+        ],
+        
+        browsers: [
+        ],
+        
+        plugins: [
+        ]
+    });
+};
