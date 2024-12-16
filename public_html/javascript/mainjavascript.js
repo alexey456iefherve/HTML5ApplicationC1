bfec32d020dc0ca582009f8a813136caeefbcfe4
alex <gellioss15@gmail.com>
2024-12-16 21:11:52 +0700

    Добавлено заново страница входа, и navbar.

---
+++ public_html/javascript/mainjavascript.js
@@ -0,0 +1,37 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+        // Script to open and close sidebar
+        function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+        function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
+        
+        
+         function w3_open() {
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("myOverlay").style.display = "block";
+        }
+
+        function w3_close() {
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("myOverlay").style.display = "none";
+        }
+         // Modal Image Gallery
+        function onClick(element) {
+            document.getElementById("img01").src = element.src;
+            document.getElementById("modal01").style.display = "block";
+            var captionText = document.getElementById("caption");
+            captionText.innerHTML = element.alt;
+        }
+       
+   
+
+    
